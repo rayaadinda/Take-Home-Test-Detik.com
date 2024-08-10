a654aff78b1ac95bc68917eec46602eb830132fa
rayaadinda <rayaadinda78@gmail.com>
2024-08-10 22:17:28 +0700

    add Hamburger menu

--- script.js
+++ script.js
@@ -21,3 +21,13 @@ const swiper = new Swiper(".testi-slider", {
 		},
 	},
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+	const hamburger = document.querySelector(".hamburger-menu")
+	const navMenu = document.querySelector(".nav-menu")
+
+	hamburger.addEventListener("click", function () {
+		navMenu.classList.toggle("active")
+		hamburger.classList.toggle("active")
+	})
+})
